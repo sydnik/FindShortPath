8251cdad297defdda18c99d7305029660eebfe78
sydnik <sydnik3@gmail.com>
2021-05-11 14:40:43 +0300

    Update Rover.java
    Нашел баг. Если  взять таблицу прямоугольник то ниего не раотало. В общем исправил и заодно облегчил код. Остался последний рывок. Нужно сохранить путь и его вывести

--- Rover.java
+++ Rover.java
@@ -4,14 +4,18 @@ public class Rover {
 
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
-//        int map[][] =   {{1, 2, 1, 9, 9},
-//                         {1, 4, 3, 9, 1},
-//                         {3, 6, 1, 5, 6},
-//                         {1, 5, 9, 2, 3},
-//                         {1, 5, 7, 3, 1}};
-        int map[][] =    {{3, 4, 5},
-                        {7, 1, 4},
-                         {2, 5, 6}};
+        int map[][] =   {{1, 2, 1, 9, 9},
+                         {1, 4, 3, 9, 1},
+                         {3, 6, 1, 5, 6},
+                         {1, 5, 9, 2, 3},
+                         {1, 5, 7, 3, 1}};
+//        int map[][] =    {{1, 5, 9},
+//                        {1, 5, 9},
+//                         };
+//        int map[][] =    {{ 4, 5},
+//                         { 1, 4},
+//                        { 5, 6}};
+//
         calculateRoverPath(map);
         System.out.println(System.currentTimeMillis()-time);
     }
@@ -19,8 +23,7 @@ public class Rover {
 
     private static int[][] staticMap = null;
     private static int maxStep = 0;
-    private static int[][][] matrixForSolution;
-//    private static int[][][] matrixCopy;
+    private static int[][][] matrixForSolution;//.z(0)-потрачено топливо z(1) - шаги все остальное z- будет собирать путь(кординаты по котором шел)
     private static int maxInt = 99;
 
 
@@ -33,23 +36,23 @@ public class Rover {
 
 
 
-
-
-
                 for(int i =0;i<matrixForSolution.length;i++){
                     for(int j =0;j<matrixForSolution.length;j++){
                     System.out.print(matrixForSolution[i][j][0]+" ");
                 }
                 System.out.println();
             }
+                for(int i=0;i<matrixForSolution[1][matrixForSolution.length-1].length;i+=2){
+                    System.out.println(matrixForSolution[1][matrixForSolution.length-1][i]+" "+matrixForSolution[1][matrixForSolution.length-1][i+1]);
+                }
+        System.out.println(matrixForSolution[1][matrixForSolution.length-1][1]);
+//
         }
 
 
-
-
-
+        //Делаю таблицу и заплднгяю ее 0 и большими числами.
     private static void createMatrix(int [][] map){
-        matrixForSolution = new int[map.length*map[0].length+1][map.length *map[0].length+1][3];
+        matrixForSolution = new int[map.length*map[0].length+1][map.length *map[0].length+1][staticMap.length*staticMap[0].length*2+2];
         for(int y =1;y<matrixForSolution.length;y++){
             for(int x =1;x<matrixForSolution[0].length;x++){
                 matrixForSolution[y][x][0] = maxInt;
@@ -83,42 +86,72 @@ public class Rover {
     }
     //Создаю таблицу и делаю из каждой ячейки 1 шаг. И заполняю данные.
     private static void fillOneStepMatrix(){
-        for(int i =0; i<staticMap.length*staticMap[0].length;i++) {
-
+        for(int i =0; i<staticMap.length*staticMap[0].length+1;i++) {
                     try {
-                        matrixForSolution[1+i][1+i+1][0] = 1 + moduleNumber(staticMap[i/staticMap.length][i%staticMap[0].length] - staticMap[i/staticMap.length][i%staticMap[0].length+1]);
+                        matrixForSolution[1+i][1+i+1][0] = 1 + moduleNumber(staticMap[i/staticMap[0].length][i%staticMap[0].length] - staticMap[i/staticMap[0].length][i%staticMap[0].length+1]);
+                        matrixForSolution[1+i+1][1+i][0] = matrixForSolution[1+i][1+i+1][0];//потрачено энергии
+
+                        matrixForSolution[1+i][1+i+1][1] = 1;
+                        matrixForSolution[1+i+1][1+i][1] = 1;//шаги
+
+                        matrixForSolution[1+i][1+i+1][2] = i/staticMap.length;
+                        matrixForSolution[1+i][1+i+1][3] = i%staticMap[0].length;
+                        matrixForSolution[1+i][1+i+1][4] = i/staticMap.length;
+                        matrixForSolution[1+i][1+i+1][5] = i%staticMap[0].length+1;//координаты
+
+                        matrixForSolution[1+i][1+i+1][2] = i/staticMap.length;
+                        matrixForSolution[1+i+1][1+i][3] = i%staticMap[0].length;
+                        matrixForSolution[1+i+1][1+i][4] = i/staticMap.length;
+                        matrixForSolution[1+i+1][1+i][5] = i%staticMap[0].length+1;//координаты
 
                     } catch (Exception e) { }
                     try {
-                        matrixForSolution[1+i][i+1-staticMap[0].length][0] = 1 + moduleNumber(staticMap[i/staticMap.length][i%staticMap[0].length] - staticMap[i/staticMap.length-1][i%staticMap[0].length]);
+                        matrixForSolution[i+1][i+1+staticMap[0].length][0] = 1 + moduleNumber(staticMap[i/staticMap[0].length][i%staticMap[0].length] - staticMap[i/staticMap[0].length+1][i%staticMap[0].length]);
+                        matrixForSolution[i+1+staticMap[0].length][i+1][0] = matrixForSolution[i+1][i+1+staticMap[0].length][0];
 
-                    } catch (Exception e) { }
-                    try {
-                        matrixForSolution[i+1][i+1+staticMap[0].length][0] = 1 + moduleNumber(staticMap[i/staticMap.length][i%staticMap[0].length] - staticMap[i/staticMap.length+1][i%staticMap[0].length]);
-                    } catch (Exception e) { }
-                    try {
-                        matrixForSolution[i+1+1][i+1][0] = 1 + moduleNumber(staticMap[i/staticMap.length][i%staticMap[0].length] - staticMap[i/staticMap.length][i%staticMap[0].length+1]);
+                        matrixForSolution[i+1][i+1+staticMap[0].length][1] = 1;
+                        matrixForSolution[i+1+staticMap[0].length][i+1][1] = 1;
 
+                        matrixForSolution[i+1][i+1+staticMap[0].length][2] = i/staticMap.length;
+                        matrixForSolution[i+1][i+1+staticMap[0].length][3] = i%staticMap[0].length;
+                        matrixForSolution[i+1][i+1+staticMap[0].length][4] = i/staticMap.length+1;
+                        matrixForSolution[i+1][i+1+staticMap[0].length][5] = i%staticMap[0].length;
+
+                        matrixForSolution[i+1+staticMap[0].length][i+1][2] = i/staticMap.length;
+                        matrixForSolution[i+1+staticMap[0].length][i+1][3] = i%staticMap[0].length;
+                        matrixForSolution[i+1+staticMap[0].length][i+1][4] = i/staticMap.length+1;
+                        matrixForSolution[i+1+staticMap[0].length][i+1][5] = i%staticMap[0].length;
                     } catch (Exception e) { }
         }
 
-    }
-    private static void fillMatrix(){
 
+
+    }
+    //узнаю самый дешевый путь
+    private static void fillMatrix(){
 
         for(int i= 1;i<matrixForSolution.length;i++) {
             for (int y = 1; y < matrixForSolution.length; y++) {
                 for (int x = 1; x < matrixForSolution.length; x++) {
-                        matrixForSolution[y][x][0] = min(matrixForSolution[y][x][0], matrixForSolution[y][i][0] + matrixForSolution[i][x][0]);
+                    int first =1;
+                    if(matrixForSolution[y][x][0]!=min(matrixForSolution[y][x][0], matrixForSolution[y][i][0] + matrixForSolution[i][x][0])) {
+                        matrixForSolution[y][x][0] = matrixForSolution[y][i][0] + matrixForSolution[i][x][0];
+                        matrixForSolution[y][x][1] = matrixForSolution[y][i][1] + matrixForSolution[i][x][1];
+                        for(first = 1;first<matrixForSolution[y][i][1]+2;first++){
+                            matrixForSolution[y][x][first*2] = matrixForSolution[y][i][first*2];
+                            matrixForSolution[y][x][first*2+1] = matrixForSolution[y][i][first*2+1];
+
+                        }
+                        for(int second =0; second<matrixForSolution[i][x][1];second++){
+                            matrixForSolution[y][x][first+second*2] = matrixForSolution[i][x][second*2];
+                            matrixForSolution[y][x][first+second*2+1] = matrixForSolution[i][x][second*2+1];
+                        }
+
+                    }
 
                     }
                 }
-            for (int p = 0; p < matrixForSolution.length; p++) {
-                for (int q = 0; q < matrixForSolution.length; q++) {
-                    System.out.print(matrixForSolution[p][q][0] + " ");
-                }
-                System.out.println();
-            }
+
             }
 
 
@@ -136,14 +169,6 @@ public class Rover {
             return -i;
         }
         return i;
-    }
-    private static int[][] copyList (int[][] list){
-        int[][] newChecklist = new int[maxStep-1][2];
-        for(int i =0;i<maxStep-1;i++){
-            newChecklist[i][0] = list[i][0];
-            newChecklist[i][1] = list[i][1];
-        }
-        return newChecklist;
     }
 
 
