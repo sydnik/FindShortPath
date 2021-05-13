a919a1688af74c9a523bfc55be56db75c8a0481d
sydnik <sydnik3@gmail.com>
2021-05-13 10:48:21 +0300

    Финальная версия
    С алгоритмом Флойда-Уоршелла не получилось. Проблема если  нужно идти в левую сторону больше чем на один шаг. и тоже самое если вверх та же проблема. теряется путь. Возвращаю первую версию которую писал с помощью рекурсии. В ней удалил лишнюю проверку.

--- Rover.java
+++ Rover.java
@@ -1,189 +1,178 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Rover {
+    private static int[][] staticMap = null;
+    private static int maxStep = 0;
 
-    public static void main(String[] args) {
-        long time = System.currentTimeMillis();
-        int map[][] =   {{1, 1, 1, 1, 1, 1, 1, 1},
-                         {9, 9, 9, 9, 9, 9, 9, 1},
-                         {1, 1, 1, 1, 1, 1, 1, 1},
-                         {1, 9, 9, 9, 9, 2, 9, 9},
-                         {1, 1, 1, 1, 1, 4, 9, 1},
-                         {9, 9, 9, 9, 1, 2, 1, 9},
-                         {1, 5, 7, 3, 1, 1, 1, 1},
-                         {1, 5, 7, 3, 1, 1, 1, 1}};
+    public static void calculateRoverPath(int[][] map) {
+        staticMap = map;
+        maxStep = (map[0].length*map.length) - 1;
+        int[] first;
+        int[] second;
+        int[][] checkList = new int[maxStep-1][2];
+        StringBuilder result = new StringBuilder("");
+        first = reflectionFindPath(map[0][1],1, new int[]{0,1},new int[]{0,0},checkList);
+        second = reflectionFindPath(map[1][0],1, new int[]{1,0},new int[]{0,0},checkList);
 
+        if(first[0]<second[0]){
+            result.append("[").append(first[2]).append("][").append(first[3]).append("]");
+            for (int i = 4;i<first.length;i = i+2){
+                result.append("->[").append(first[i]).append("][").append(first[i+1]).append("]");
+            }
+            result.append("\nsteps: ").append(first[1])
+                    .append("\nfuel: ").append(first[0]);
+        }
+        else {
+            result.append("[").append(second[2]).append("][").append(second[3]).append("]");
+            for (int i = 4;i<second.length;i = i+2){
+                result.append("->[").append(second[i]).append("][").append(second[i+1]).append("]");
+            }
+            result.append("\nsteps: ").append(second[1])
+                    .append("\nfuel: ").append(second[0]);
 
-        calculateRoverPath(map);
-        System.out.println(System.currentTimeMillis()-time);
+        }
+        try(FileWriter writer = new FileWriter("path-plan.txt")) {
+            writer.write(String.valueOf(result));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
-        private static int[][] staticMap = null;
-        private static int[][][] matrixForSolution;//.z(0)-потрачено топливо z(1) - шаги все остальное z- будет собирать путь(кординаты по котором шел)
-        private static int maxInt = 9999999;
-        private static int[][] shortPath = null;
 
-
-
-        public static void calculateRoverPath(int[][] map) {
-            staticMap=map;
-            createMatrix();
-            fillOneStepMatrix();
-        for(int i =0;i<matrixForSolution.length;i++){
-            for(int j =0;j<matrixForSolution.length;j++){
-                System.out.print(matrixForSolution[i][j][0]+" ");
-            }
-            System.out.println();
+    //первое число количество топлива, второй количество шагов, и далее список адресов
+    private static int[] reflectionFindPath(int height,int step,int [] address,int [] startAddress,int[][] checklist){
+        if(address[0]==staticMap.length-1&&address[1]==staticMap[0].length-1){
+            int [] result = new int[2+((step+1)*2)];
+            result[1] = step;
+            result [0] = moduleNumber(staticMap[startAddress[0]][startAddress[1]]-staticMap[address[0]][address[1]])+step;
+            result[(1+step)*2+1]=address[1];
+            result[(1+step)*2]=address[0];
+            return result;
         }
-            fillMatrix();
-            findShortPath();
-
-
-
-                for(int i =0;i<matrixForSolution.length;i++){
-                    for(int j =0;j<matrixForSolution.length;j++){
-                    System.out.print(matrixForSolution[i][j][0]+" ");
+        //Проверки что путь не зашел на ячейку где  он уже был и не стоит ли он  рядом в клетке с путем которым он был
+        for(int i=0;i<step;i++){
+                if (address[0] == checklist[i][0] && address[1] == checklist[i][1]) {
+                    return new int[]{Integer.MAX_VALUE};
                 }
-                System.out.println();
-            }
-                for(int i=0;i<matrixForSolution[1][matrixForSolution.length-1].length;i++){
-                    System.out.println(matrixForSolution[1][matrixForSolution.length-1][i]);
-                }
-                for(int i = 0;i<shortPath.length;i++){
-                    System.out.println(Arrays.toString(shortPath[i]));
-                }
-        System.out.println(matrixForSolution[matrixForSolution.length-1][1][1]);
-//
-        }
-
-
-        //Делаю таблицу и заплднгяю ее 0 и большими числами.
-        private static void createMatrix(){
-        matrixForSolution = new int[staticMap.length*staticMap[0].length+1][staticMap.length *staticMap[0].length+1][3];
-        for(int y =1;y<matrixForSolution.length;y++){
-            for(int x =1;x<matrixForSolution[0].length;x++){
-                matrixForSolution[y][x][0] = maxInt;
-            }
-
-        }
-        for(int y =1;y<staticMap.length;y++){
-            for(int x =1;x<staticMap[0].length;x++){
-                matrixForSolution[y][x][0] = maxInt;
-            }
-
-        }
-        int y=0;
-        int x = 0;
-        for(int i = 1;i<matrixForSolution.length;i++){
-
-            matrixForSolution[i][i][0] = 0;
-            matrixForSolution[i][0][0] = i;
-            matrixForSolution[0][i][0] = i;
-            matrixForSolution[i][0][1] = y;
-            matrixForSolution[0][i][1] = y;
-            matrixForSolution[0][i][2] = x;
-            matrixForSolution[i][0][2] = x;
-            x++;
-            if(i%staticMap.length==0){
-                y++;
-                x =0;
-            }
-        }
-
-    }
-        //Создаю таблицу и делаю из каждой ячейки 1 шаг. И заполняю данные.
-        private static void fillOneStepMatrix(){
-        for(int i =0; i<staticMap.length*staticMap[0].length+1;i++) {
-                    try {
-                        matrixForSolution[1+i][1+i+1][0] = 1 + moduleNumber(staticMap[i/staticMap[0].length][i%staticMap[0].length] - staticMap[i/staticMap[0].length][i%staticMap[0].length+1]);
-                        matrixForSolution[1+i+1][1+i][0] = matrixForSolution[1+i][1+i+1][0];//потрачено энергии
-
-                        matrixForSolution[1+i][1+i+1][1] = 1;
-                        matrixForSolution[1+i+1][1+i][1] = 1;//шаги
-
-                        matrixForSolution[1+i][1+i+1][2] = i+1;//координаты
-
-                        matrixForSolution[1+i+1][1+i][2] = i+1;//координаты
-
-                    } catch (Exception e) { }
-                    try {
-                        matrixForSolution[i+1][i+1+staticMap[0].length][0] = 1 + moduleNumber(staticMap[i/staticMap[0].length][i%staticMap[0].length] - staticMap[i/staticMap[0].length+1][i%staticMap[0].length]);
-                        matrixForSolution[i+1+staticMap[0].length][i+1][0] = matrixForSolution[i+1][i+1+staticMap[0].length][0];
-
-                        matrixForSolution[i+1][i+1+staticMap[0].length][1] = 1;
-                        matrixForSolution[i+1+staticMap[0].length][i+1][1] = 1;
-
-                        matrixForSolution[i+1][i+1+staticMap[0].length][2] = i+1;
-                        matrixForSolution[i+1+staticMap[0].length][i+1][2] = 1+i+staticMap[0].length;
-
-                    } catch (Exception e) { }
-        }
-
-
-
-    }
-        //узнаю самый дешевый путь
-        private static void fillMatrix(){
-
-        for(int i= 1;i<matrixForSolution.length;i++) {
-            for (int y = 1; y < matrixForSolution.length; y++) {
-                for (int x = 1; x < matrixForSolution.length; x++) {
-//                    if(x==i||y==i){
-//                        continue;
-//                    }
-                    if(matrixForSolution[y][x][0]>min(matrixForSolution[y][x][0], matrixForSolution[y][i][0] + matrixForSolution[i][x][0])) {
-                        matrixForSolution[y][x][0] = matrixForSolution[y][i][0] + matrixForSolution[i][x][0];
-                        matrixForSolution[y][x][1] = matrixForSolution[y][i][1] + matrixForSolution[i][x][1];
-//Может косяк в минимкм? Есть косяк 3 строчку ссылается не на те ячейки и пишет не правильный путь
-                            matrixForSolution[y][x][2] = i;
+                try {
+                    if (address[0] - 1 == checklist[i][0] && address[1] == checklist[i][1]&&startAddress[0]!=address[0] - 1&& startAddress[1]!=address[1]) {
+                        return new int[]{Integer.MAX_VALUE};
                     }
-
-                    }
+                }catch (Exception e){}
+            try {
+                if (address[0] + 1 == checklist[i][0] && address[1] == checklist[i][1]&&startAddress[0]!=address[0] + 1&& startAddress[1]!=address[1]) {
+                    return new int[]{Integer.MAX_VALUE};
                 }
-                if(i==21){
-                    for(int b =0;b<matrixForSolution.length;b++){
-                        for(int a =0;a<matrixForSolution.length;a++){
-                            System.out.print(matrixForSolution[b][a][0]+" ");
-                        }
-                        System.out.println();
-                    }
+            }catch (Exception e){}
+            try {
+                if (address[0]  == checklist[i][0] && address[1]-1 == checklist[i][1]&&startAddress[0]!=address[0]&& startAddress[1]!=address[1]-1) {
+                    return new int[]{Integer.MAX_VALUE};
                 }
-            }
-
-
-
-        }
-        //Нахожу кратчайший путь и записывают в статик
-        private static void findShortPath(){
-        int[] list = new int[matrixForSolution[1][matrixForSolution.length-1][1]+1];// Список адресов не в ту стороны
-            list[0] = matrixForSolution[1].length-1;
-
-            for (int i = 1;i < matrixForSolution[1][matrixForSolution.length-1][1]+1;i++){
-                list[i] = matrixForSolution[1][list[i-1]][2];
-            }
-            shortPath = new int [list.length][2];
-            int k =0;
-            for(int i=list.length-1;i>= 0;i--){
-                shortPath[i][0] = (list[k]-1)/staticMap[0].length;//-1 делаю т.к. для себя добавлял номер в 0 строку/столбец
-                shortPath[i][1] = (list[k]-1)%staticMap[0].length;
-                k++;
-            }
+            }catch (Exception e){}
+            try {
+                if (address[0] == checklist[i][0] && address[1]+1 == checklist[i][1]&&startAddress[0]!=address[0]&& startAddress[1]!=address[1]+1) {
+                    return new int[]{Integer.MAX_VALUE};
+                }
+            }catch (Exception e){}
 
         }
 
-        private static int min (int first,int second){
-            if(first>second){
-                return second;
-            }
-            return  first;
+
+        int stepNew = step;
+        int[] first = new int[1];
+        int[] second = new int[1];
+        int[] third = new int[1];
+        int newAddress[][] = new int[3][2];
+        int[][] newCheckList = copyList(checklist);
+
+        newCheckList[step][0] = address[0];
+        newCheckList[step][1] = address[1];
+        int check = 0;
+        if(address[0]+1!=startAddress[0]||address[1]!=startAddress[1]){
+            newAddress[check][0] = address[0]+1;
+            newAddress[check][1] = address[1];
+            check++;
+        }
+
+        if(address[0]!=startAddress[0]||address[1]-1!=startAddress[1]){
+            newAddress[check][0] = address[0];
+            newAddress[check][1] = address[1]-1;
+            check++;
+        }
+        if(address[0]!=startAddress[0]||address[1]+1!=startAddress[1]){
+            newAddress[check][0] = address[0];
+            newAddress[check][1] = address[1]+1;
+            check++;
+        }
+        if (address[0]-1!=startAddress[0]||address[1]!=startAddress[1]){
+            newAddress[check][0] = address[0] - 1;
+            newAddress[check][1] = address[1];
+        }
+
+        try {
+            first = reflectionFindPath(staticMap[startAddress[0]][startAddress[1]],stepNew+1, new int[]{newAddress[0][0],newAddress [0][1]},address,newCheckList);
+        }catch (Exception e){ first[0] = Integer.MAX_VALUE;
+        }
+        try {
+            second = reflectionFindPath(staticMap[startAddress[0]][startAddress[1]],stepNew+1, new int[]{newAddress[1][0],newAddress [1][1]},address,newCheckList);
+        }catch (Exception e){second[0] = Integer.MAX_VALUE;
+        }
+        try {
+            third = reflectionFindPath(staticMap[startAddress[0]][startAddress[1]],stepNew+1, new int[]{newAddress[2][0],newAddress [2][1]},address,newCheckList);
+        }catch (Exception e){third[0] = Integer.MAX_VALUE;
+        }
+
+        if(first.length!=1){
+            first[2+(step)*2+1]=address[1];
+            first[2+(step)*2]=address[0];
+            first[0] = first[0] + moduleNumber(staticMap[startAddress[0]][startAddress[1]]-staticMap[address[0]][address[1]]);
+        }
+        if(second.length!=1){
+            second[2+(step)*2+1]=address[1];
+            second[2+(step)*2]=address[0];
+            second[0] = second[0] + moduleNumber(staticMap[startAddress[0]][startAddress[1]]-staticMap[address[0]][address[1]]);
+        }
+        if(third.length!=1){
+            third[2+(step)*2+1]=address[1];
+            third[2+(step)*2]=address[0];
+            third[0] = third[0] + moduleNumber(staticMap[startAddress[0]][startAddress[1]]-staticMap[address[0]][address[1]]);
+        }
+
+        if(first[0]==second[0]&&second[0]==third[0]){
+            return first;
+        }
+        else if(first[0]<second[0]&&first[0]<third[0]){
+            return first;
+        }
+        else if(second[0]<first[0]&&second[0]<third[0]){
+            return second;
+        }
+        else if(third[0]<second[0]&&third[0]<first[0]){
+            return third;
+        }
+        else if(first[0]<second[0]||first[0]<third[0]){
+            return first;
+        }
+        else if(second[0]<third[0]||second[0]<first[0]){
+            return second;
+        }
+        return third;
     }
-        private static int moduleNumber (int i){
+    private static int moduleNumber (int i){
         if(i<0){
             return -i;
         }
         return i;
+    }
+    private static int[][] copyList (int[][] list){
+        int[][] newChecklist = new int[maxStep-1][2];
+        for(int i =0;i<maxStep-1;i++){
+            newChecklist[i][0] = list[i][0];
+            newChecklist[i][1] = list[i][1];
+        }
+        return newChecklist;
     }
 
 
